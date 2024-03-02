aa5a025751e46915a28cabc4c38188c5885b72eb
HannaMalygina <gannamalygina@gmail.com>
2024-03-02 21:57:12 +0100

    total_salary first version

---
+++ total_salary.py
@@ -0,0 +1,25 @@
+def total_salary(path):
+
+    sum = 0
+    ave = 0
+    nof_empl = 0
+    lines = []
+    try:
+        with open (path, "r", encoding = "utf-8") as fh:
+            lines = [el for el in fh.readlines()]
+    except FileNotFoundError:
+        print ("File {path} was not found")
+    for line in lines:
+        if line: 
+            try:
+                sum += float(line.split(",")[1])
+            except IndexError:
+                print("A line in the file has wrong format. Must be: 'name','salary'. I go to the next line")
+                continue
+            nof_empl += 1
+    if nof_empl > 0: ave = sum / nof_empl
+    
+    return sum, ave
+
+total, average = total_salary("sal.txt")
+print (f"{total = } {average = }")
